d39f615091e2ff943c443cb9697fe6058179fa4f
Chris Williams <cwill833@gmail.com>
2019-10-28 10:29:58 -0500

    create function that calculates how much fuel is needed based on string arg and a function that tells us how much fuel is left in the tank

--- travel.go
+++ travel.go
@@ -2,7 +2,28 @@ package main
 
 import "fmt"
 
-func main(){
+func fuelGuage(fuel int){
+	fmt.Printf("You have %d gallons left in the tank", fuel)
+}
 
-	
+func calculateFuel(planet string) int{
+	var fuel int
+
+	switch planet {
+	case "Venus":
+		fuel = 300000
+	case "Mercury":
+		fuel = 500000
+	case "Mars":
+		fuel = 700000
+	default:
+		fuel = 0
+	}
+
+	return fuel
+}
+
+func main(){
+	fmt.Println(calculateFuel("Mars"))
+	fuelGuage(100)
 }
